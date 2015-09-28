cf9ecc6164168e6d8762072b1874f53dd53fddb4
Rob Johnson <rob.johnson@holidayextras.com>
2015-09-28 14:53:32 +0100

    No triple equals in bash

--- nodeApps/postRelease.sh
+++ nodeApps/postRelease.sh
@@ -4,7 +4,7 @@ CANDIDATE_VERSION=`cat package.json | jq '.version' | tr -d '"'`
 REPO_OWNER="holidayextras"
 ENVIRONMENT=$1
 
-if [ $ENVIRONMENT === "production" ]; then
+if [ $ENVIRONMENT = "production" ]; then
 	curl --user "${GHUSER}:${GHPASS}" --data "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by @${CIRCLE_USERNAME} (via build: [${CIRCLE_BUILD_NUM}](https://circleci.com/gh/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/releases
 fi
 echo "${GRAPHITE_API_KEY}.counters.${APP_NAME}.${ENVIRONMENT}.inf.deployment 1" | nc ${GRAPHITE_ENDPOINT_PREFIX}.carbon.hostedgraphite.com 2003
