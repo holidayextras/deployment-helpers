abdacedd3ce80653c70f914137c07c6a1c25bd69
Rob Johnson <rob.johnson@holidayextras.com>
2015-09-28 14:44:36 +0100

    No reelase support

--- nodeApps/preRelease.sh
+++ nodeApps/preRelease.sh
@@ -13,19 +13,23 @@ echo "Local Version: $CANDIDATE_VERSION_STRING"
 RELEASED_VERSION=(${RELEASED_VERSION_STRING//./ })
 CANDIDATE_VERSION=(${CANDIDATE_VERSION_STRING//./ })
 
-# Check if the major is less than
-if [ ${CANDIDATE_VERSION[0]} -lt ${RELEASED_VERSION[0]} ]; then
-	# e.g. 1.0.0 < 2.0.0
-	exit 1
-fi
-# Check if the major is less than or equal and if the minor is less than
-if [ ${CANDIDATE_VERSION[0]} -le ${RELEASED_VERSION[0]} ] && [ ${CANDIDATE_VERSION[1]} -lt ${RELEASED_VERSION[1]} ]; then
-	# e.g. 1.0.0 < 1.1.0
-	exit 1
-fi
-# Check if the major is less than or equal and if the minor is less than or equal and if the patch is less than or equal
-if [ ${CANDIDATE_VERSION[0]} -le ${RELEASED_VERSION[0]} ] && [ ${CANDIDATE_VERSION[1]} -le ${RELEASED_VERSION[1]} ] && [ ${CANDIDATE_VERSION[2]} -le ${RELEASED_VERSION[2]} ]; then
-	# e.g. 1.0.0 < 1.0.1
-	exit 1
+if [ $RELEASED_VERSION = "null" ]; then
+	echo "No release to date"
+else
+	# Check if the major is less than
+	if [ ${CANDIDATE_VERSION[0]} -lt ${RELEASED_VERSION[0]} ]; then
+		# e.g. 1.0.0 < 2.0.0
+		exit 1
+	fi
+	# Check if the major is less than or equal and if the minor is less than
+	if [ ${CANDIDATE_VERSION[0]} -le ${RELEASED_VERSION[0]} ] && [ ${CANDIDATE_VERSION[1]} -lt ${RELEASED_VERSION[1]} ]; then
+		# e.g. 1.0.0 < 1.1.0
+		exit 1
+	fi
+	# Check if the major is less than or equal and if the minor is less than or equal and if the patch is less than or equal
+	if [ ${CANDIDATE_VERSION[0]} -le ${RELEASED_VERSION[0]} ] && [ ${CANDIDATE_VERSION[1]} -le ${RELEASED_VERSION[1]} ] && [ ${CANDIDATE_VERSION[2]} -le ${RELEASED_VERSION[2]} ]; then
+		# e.g. 1.0.0 < 1.0.1
+		exit 1
+	fi
 fi
 exit 0
