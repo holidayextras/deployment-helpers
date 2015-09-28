bab3696a257c1861498eb42cebdd433cd22a4754
Rob Johnson <rob.johnson@holidayextras.com>
2015-09-28 15:08:38 +0100

    More commentsgit add .

--- nodeApps/postRelease.sh
+++ nodeApps/postRelease.sh
@@ -5,6 +5,8 @@ REPO_OWNER="holidayextras"
 ENVIRONMENT=$1
 
 if [ $ENVIRONMENT = "production" ]; then
+	# Tag this deploy and ake a release in github
 	curl --user "${GHUSER}:${GHPASS}" --data "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by @${CIRCLE_USERNAME} (via build: [${CIRCLE_BUILD_NUM}](https://circleci.com/gh/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/releases
 fi
+# Add a deployment counter in the metrics platform (graphite)
 echo "${GRAPHITE_API_KEY}.counters.${APP_NAME}.${ENVIRONMENT}.inf.deployment 1" | nc ${GRAPHITE_ENDPOINT_PREFIX}.carbon.hostedgraphite.com 2003
