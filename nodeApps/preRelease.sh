90cce1570999435b335aca9c3d814d3551a720ca
Rob Johnson <rob.johnson@holidayextras.com>
2015-09-30 09:03:39 +0100

    Better git tag getting

--- nodeApps/preRelease.sh
+++ nodeApps/preRelease.sh
@@ -4,7 +4,7 @@ REPO_OWNER="holidayextras"
 APP_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
 # Get the last version from Git
 git fetch
-RELEASED_VERSION_STRING=`git describe origin/master --tags | tr -d "v"`
+RELEASED_VERSION_STRING=`git describe --tags `git rev-list --tags --max-count=1` | tr -d "v"`
 # Get the version from the local package.json
 CANDIDATE_VERSION_STRING="`cat package.json | grep version | cut -d '"' -f 4`"
 
