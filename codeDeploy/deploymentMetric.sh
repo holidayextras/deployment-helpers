4157c247623a99c56f392919cbf45b0fee8783f0
Rob Johnson <rob.johnson@holidayextras.com>
2015-10-30 10:24:33 +0000

    DEPLOYMENT_GROUP_NAME, not NODE_ENV

--- codeDeploy/deploymentMetric.sh
+++ codeDeploy/deploymentMetric.sh
@@ -1,1 +1,2 @@
-echo "counters.$APPLICATION_NAME.$NODE_ENV.inf.deployment 1|c" | nc -u -w0 127.0.0.1 8125
+echo "counters.${APPLICATION_NAME}.${DEPLOYMENT_GROUP_NAME,,}.inf.deployment 1|c"
+echo "counters.${APPLICATION_NAME}.${DEPLOYMENT_GROUP_NAME,,}.inf.deployment 1|c" | nc -u -w0 127.0.0.1 8125
