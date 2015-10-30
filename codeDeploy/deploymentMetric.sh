33fccc5719fe18004d02050b904820cad3a28cde
Rob Johnson <rob.johnson@holidayextras.com>
2015-10-30 11:30:52 +0000

    Metric naming

--- codeDeploy/deploymentMetric.sh
+++ codeDeploy/deploymentMetric.sh
@@ -1,3 +1,3 @@
 ENVIRONMENT=$(echo "$DEPLOYMENT_GROUP_NAME" | tr '[:upper:]' '[:lower:]')
-echo "counters.${APPLICATION_NAME}.${ENVIRONMENT}.inf.deployment 1|c"
-echo "counters.${APPLICATION_NAME}.${ENVIRONMENT}.inf.deployment 1|c" | nc -u -w0 127.0.0.1 8125
+echo "counters.${APPLICATION_NAME}.${ENVIRONMENT}.inf.deployment:1|c"
+echo "counters.${APPLICATION_NAME}.${ENVIRONMENT}.inf.deployment:1|c" | nc -u -w0 127.0.0.1 8125
