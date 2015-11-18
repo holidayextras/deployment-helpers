609ed5e444be37f21137daed038011f3a91dbe82
Marc Sygrove <tigrou.m@gmail.com>
2015-11-18 10:49:14 +0000

    changed the service name to use the primaryApplication by default

--- codeDeploy/restartService.sh
+++ codeDeploy/restartService.sh
@@ -2,7 +2,14 @@
 SERVICE=${APPLICATION_NAME}
 
 if [ ${APPLICATION_NAME} = "themeBlueprint" ] || [ ${APPLICATION_NAME} = "theatreBlueprint" ]; then
-	SERVICE="the-wall"
+  SERVICE="the-wall"
 fi
-echo "Restarting $SERVICE"
+
+file="/etc/init/${SERVICE}.conf"
+if [ ! -f "$file" ]; then
+  # fall back to the default
+  SERVICE="primaryApplication"
+fi
+
+echo "Restarting ${SERVICE} (${APPLICATION_NAME})"
 service $SERVICE restart
