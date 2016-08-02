3264db2d18fb9f555476b1d2e236975b2ec651d2
Paul Clarke <paul.clarke@holidayextras.com>
2016-08-02 11:45:14 +0100

    :mouse: typo in warning message

--- nodeApps/postRelease.sh
+++ nodeApps/postRelease.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $0 is depracated please update to createPrivateRelease.sh instead
+echo $0 is deprecated please update to createPrivateRelease.sh instead
 pwd
 ${DIR}/createPrivateRelease.sh $1 $2
 exit 0
