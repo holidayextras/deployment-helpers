65804505850cba996f10f93e953bf7bdf36e3a21
Rob Johnson <rob.johnson@holidayextras.com>
2017-04-28 09:49:16 +0100

    PR Comments

--- ECS/SBP/deploy.sh
+++ ECS/SBP/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-while getopts ":c:m:p:" opt; do
+DESIRED_COUNT=1
+while getopts ":c:m:p:i:" opt; do
   case $opt in
     c) CPU="$OPTARG"
     ;;
+    i) DESIRED_COUNT=$OPTARG
+	;;
     m) MEMORY="$OPTARG"
     ;;
     p) PORT="$OPTARG"
@@ -16,14 +19,14 @@ done
 echo "CPU=${CPU}"
 echo "MEMORY=${MEMORY}"
 echo "PORT=${PORT}"
+echo "DESIRED_COUNT=${DESIRED_COUNT}"
 
-VPC_ID="vpc-9f48f7fa"
-VPC_SUBNETS="subnet-7916a320 subnet-a69af1c3 subnet-e1a03396"
-VPC_SECURITY_GROUPS="sg-cad732ad"
+VPC_ID="${AWS_VPC_ID}"
+VPC_SUBNETS="${AWS_VPC_SUBNETS}"
+VPC_SECURITY_GROUPS="${AWS_VPC_SECURITY_GROUPS}"
 
 SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
 CLUSTER_NAME="SBP-Cluster-1"
-DESIRED_COUNT=1
 IMAGE_TAG=${CIRCLE_SHA1:-latest}
 TEMPLATE_FILE="$(dirname "$0")/ecs-task-definition"
 $(aws ecr get-login --region eu-west-1)
@@ -48,16 +51,16 @@ SERVICE_RESPONSE=`aws ecs describe-services --cluster ${CLUSTER_NAME} --services
 echo "SERVICE_RESPONSE: ${SERVICE_RESPONSE}"
 if [[ ${SERVICE_RESPONSE} = "MISSING" ]]; then
 	if [ -z "${PORT}" ]; then
-		echo "SERVICE: `aws ecs create-service --cluster ${CLUSTER_NAME} --task-definition ${SERVICE_NAME}:${TASK_REVISION} --desired-count ${DESIRED_COUNT} --service-name ${SERVICE_NAME} --query service.status --output text`"
+		echo "CREATE SERVICE: `aws ecs create-service --cluster ${CLUSTER_NAME} --task-definition ${SERVICE_NAME}:${TASK_REVISION} --desired-count ${DESIRED_COUNT} --service-name ${SERVICE_NAME} --query service.status --output text`"
 	else
-		ALB_ARN=`aws elbv2 create-load-balancer --name ${SERVICE_NAME} --subnets ${VPC_SUBNETS} --security-groups ${VPC_SECURITY_GROUPS} --query LoadBalancers[0].LoadBalancerArn --output text`
-		echo "ALB_ARN: ${ALB_ARN}"
-		TARGET_ARN=`aws elbv2 create-target-group --name ${SERVICE_NAME} --protocol HTTP --port 80 --vpc-id ${VPC_ID} --query TargetGroups[0].TargetGroupArn --output text`
-		echo "TARGET_ARN: ${TARGET_ARN}"		
-		echo "CREATE_LISTNER: `aws elbv2 create-listener --load-balancer-arn ${ALB_ARN} --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=${TARGET_ARN} --query Listners[0].ListnerArn --output text`"
-		echo "SERVICE: `aws ecs create-service --cluster ${CLUSTER_NAME} --task-definition ${SERVICE_NAME}:${TASK_REVISION} --desired-count ${DESIRED_COUNT} --service-name ${SERVICE_NAME} --role ecsServiceRole --load-balancers targetGroupArn=${TARGET_ARN},containerName=${SERVICE_NAME},containerPort=${PORT} --query service.status --output text`"
-		echo "Application URL: http://`aws elbv2 describe-load-balancers --load-balancer-arns ${ALB_ARN} --query LoadBalancers[0].DNSName --output text`"
+		APPLICATION_LOAD_BALANCER_ARN=`aws elbv2 create-load-balancer --name ${SERVICE_NAME} --subnets ${VPC_SUBNETS} --security-groups ${VPC_SECURITY_GROUPS} --query LoadBalancers[0].LoadBalancerArn --output text`
+		echo "APPLICATION_LOAD_BALANCER_ARN: ${APPLICATION_LOAD_BALANCER_ARN}"
+		TARGET_GROUP_ARN=`aws elbv2 create-target-group --name ${SERVICE_NAME} --protocol HTTP --port 80 --vpc-id ${VPC_ID} --query TargetGroups[0].TargetGroupArn --output text`
+		echo "TARGET_GROUP_ARN: ${TARGET_GROUP_ARN}"
+		echo "CREATE LISTNER: `aws elbv2 create-listener --load-balancer-arn ${APPLICATION_LOAD_BALANCER_ARN} --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=${TARGET_GROUP_ARN} --query Listners[0].ListnerArn --output text`"
+		echo "CREATE SERVICE: `aws ecs create-service --cluster ${CLUSTER_NAME} --task-definition ${SERVICE_NAME}:${TASK_REVISION} --desired-count ${DESIRED_COUNT} --service-name ${SERVICE_NAME} --role ecsServiceRole --load-balancers targetGroupArn=${TARGET_GROUP_ARN},containerName=${SERVICE_NAME},containerPort=${PORT} --query service.status --output text`"
+		echo "Application URL: http://`aws elbv2 describe-load-balancers --load-balancer-arns ${APPLICATION_LOAD_BALANCER_ARN} --query LoadBalancers[0].DNSName --output text`"
 	fi
 else
-	echo "UPDATE_SERVICE: `aws ecs update-service --cluster ${CLUSTER_NAME} --task-definition ${SERVICE_NAME}:${TASK_REVISION} --desired-count ${DESIRED_COUNT} --service ${SERVICE_NAME} --query service.status --output text`"
+	echo "UPDATE SERVICE: `aws ecs update-service --cluster ${CLUSTER_NAME} --task-definition ${SERVICE_NAME}:${TASK_REVISION} --desired-count ${DESIRED_COUNT} --service ${SERVICE_NAME} --query service.status --output text`"
 fi
