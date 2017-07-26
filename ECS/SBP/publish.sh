35310bab33f8c8bcaec6fe7e2b52974ab35747f5
Rob Johnson <rob.johnson@holidayextras.com>
2017-07-26 10:48:37 +0100

    Tag as branch

--- ECS/SBP/publish.sh
+++ ECS/SBP/publish.sh
@@ -3,7 +3,7 @@ echo "CIRCLE_BRANCH: ${CIRCLE_BRANCH}"
 echo "CI_PULL_REQUEST: ${CI_PULL_REQUEST}"
 if [ ${CIRCLE_BRANCH} = "master" ] || [ ${CIRCLE_BRANCH} = "staging" ] || [ -z ${CI_PULL_REQUEST} ]; then
 	SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
-	IMAGE_TAG=${CI_PULL_REQUEST}:-${CIRCLE_BRANCH}
+	IMAGE_TAG=${CIRCLE_BRANCH}
 	$(aws ecr get-login --region eu-west-1  --no-include-email)
 
 	docker build -t ${SERVICE_NAME} .
