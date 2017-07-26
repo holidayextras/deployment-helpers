3f9cac151efda3b8d7c9f4d3cf8e433de7fc5328
Rob Johnson <rob.johnson@holidayextras.com>
2017-07-26 11:13:34 +0100

    Simpler docker push script

--- ECS/SBP/publish.sh
+++ ECS/SBP/publish.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "CIRCLE_BRANCH: ${CIRCLE_BRANCH}"
-echo "CI_PULL_REQUEST: ${CI_PULL_REQUEST}"
 SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
 IMAGE_TAG=${CIRCLE_BRANCH}
-$(aws ecr get-login --region eu-west-1  --no-include-email)
+$(aws ecr get-login --region eu-west-1)
 
 docker build -t ${SERVICE_NAME} .
 docker tag ${SERVICE_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
