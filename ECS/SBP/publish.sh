350e01a693f84204fd989957f68cd3880c2c340c
Rob Johnson <rob.johnson@holidayextras.com>
2017-07-26 10:58:46 +0100

    Version bump

--- ECS/SBP/publish.sh
+++ ECS/SBP/publish.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "CIRCLE_BRANCH: ${CIRCLE_BRANCH}"
 echo "CI_PULL_REQUEST: ${CI_PULL_REQUEST}"
-if [ ${CIRCLE_BRANCH} = "master" ] || [ ${CIRCLE_BRANCH} = "staging" ] || [ -z ${CI_PULL_REQUEST} ]; then
-	SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
-	IMAGE_TAG=${CIRCLE_BRANCH}
-	$(aws ecr get-login --region eu-west-1  --no-include-email)
+SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
+IMAGE_TAG=${CIRCLE_BRANCH}
+$(aws ecr get-login --region eu-west-1  --no-include-email)
 
-	docker build -t ${SERVICE_NAME} .
-	docker tag ${SERVICE_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
-	docker push ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
-fi
+docker build -t ${SERVICE_NAME} .
+docker tag ${SERVICE_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
