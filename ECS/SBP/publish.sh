57dc0d1e80b2daa9c738ec2fbf5e7f982149a7e4
Rob Johnson <rob.johnson@holidayextras.com>
2017-07-26 10:25:06 +0100

    Adding conditional

--- ECS/SBP/publish.sh
+++ ECS/SBP/publish.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
-IMAGE_TAG=$1
-$(aws ecr get-login --region eu-west-1  --no-include-email)
+echo "CIRCLE_BRANCH: ${CIRCLE_BRANCH}"
+echo "CI_PULL_REQUESTS: ${CI_PULL_REQUESTS}"
+if [ ${CIRCLE_BRANCH} = "master" ] || [ ${CIRCLE_BRANCH} = "staging" ] || [ -z ${CI_PULL_REQUESTS} ]; then
+	SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
+	IMAGE_TAG=$1
+	$(aws ecr get-login --region eu-west-1  --no-include-email)
 
-docker build -t ${SERVICE_NAME} .
-docker tag ${SERVICE_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
+	docker build -t ${SERVICE_NAME} .
+	docker tag ${SERVICE_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
+	docker push ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
+fi
