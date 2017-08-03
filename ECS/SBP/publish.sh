6cc6715cb94e96010aed8645b6b505b40650e489
Rob Johnson <rob.johnson@holidayextras.com>
2017-08-03 08:47:14 +0100

    email flag not yet supported in circle

--- ECS/SBP/publish.sh
+++ ECS/SBP/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SERVICE_NAME="`cat package.json | grep -m 1 name | cut -d '"' -f 4`"
 IMAGE_TAG=${CIRCLE_BRANCH}
-$(aws ecr get-login --region eu-west-1 --no-include-email)
+$(aws ecr get-login --region eu-west-1)
 
 docker build -t ${SERVICE_NAME} .
 docker tag ${SERVICE_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${SERVICE_NAME}:${IMAGE_TAG}
