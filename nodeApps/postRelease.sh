67adfd3d33544639cf1dd04f06675f7ce0d47ecc
Jordan Hood <jordan.hood@virginmedia.com>
2018-07-24 14:35:55 +0100

    update to work on dockyard ci

--- nodeApps/postRelease.sh
+++ nodeApps/postRelease.sh
@@ -8,7 +8,7 @@ NO_METRIC=$2
 if [ $ENVIRONMENT = "production" ] || [ $ENVIRONMENT = "master" ]; then
 	# Tag this deploy and make a release in github
 	if [ $CI_PLATFORM = 'dockyard' ] ;  then
-		curl --user "${GHUSER}:${GHPASS}" --data  "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by HXCi (via build: [${CI_REPO_NAME}](https://ci.dock-yard.io/repo/${CI_REPO_NAME})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CI_REPO_NAME}/releases
+		curl --user "${GHUSER}:${GHPASS}" --data  "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by HXCi (via:[ci-service](https://ci.dock-yard.io/repo/${CI_REPO_NAME})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CI_REPO_NAME}/releases
 	else
 		curl --user "${GHUSER}:${GHPASS}" --data  "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by @${CIRCLE_USERNAME} (via build: [${CIRCLE_BUILD_NUM}](https://circleci.com/gh/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/releases
 	fi
