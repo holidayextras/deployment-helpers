2f7fe4c0ca4e90d0882a48e25294ad4ea5bbf943
Jordan Hood <jordan.hood@virginmedia.com>
2018-07-24 14:12:07 +0100

    update to work on dockyard ci

--- nodeApps/postRelease.sh
+++ nodeApps/postRelease.sh
@@ -7,7 +7,11 @@ NO_METRIC=$2
 
 if [ $ENVIRONMENT = "production" ] || [ $ENVIRONMENT = "master" ]; then
 	# Tag this deploy and make a release in github
-	curl --user "${GHUSER}:${GHPASS}" --data "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by @${CIRCLE_USERNAME} (via build: [${CIRCLE_BUILD_NUM}](https://circleci.com/gh/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/releases
+	if [ $CI_PLATFORM = 'dockyard' ] ;  then
+		curl --user "${GHUSER}:${GHPASS}" --data  "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by HXCi (via build: [${CI_BUILD_ID}](https://ci.dock-yard.io/${CI_REPO_NAME}/${CI_BUILD_ID})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CI_REPO_NAME}/releases
+	else
+		curl --user "${GHUSER}:${GHPASS}" --data  "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by @${CIRCLE_USERNAME} (via build: [${CIRCLE_BUILD_NUM}](https://circleci.com/gh/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/releases
+	fi
 fi
 # Add a deployment counter in the metrics platform (graphite)
 if [ -z $NO_METRIC ]; then
