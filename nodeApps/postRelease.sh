df331a316716b68c92352d1c052c4fa2a0a651fe
Jordan Hood <jordan.hood@virginmedia.com>
2018-07-24 14:33:06 +0100

    remove redundant code

--- nodeApps/postRelease.sh
+++ nodeApps/postRelease.sh
@@ -13,7 +13,3 @@ if [ $ENVIRONMENT = "production" ] || [ $ENVIRONMENT = "master" ]; then
 		curl --user "${GHUSER}:${GHPASS}" --data  "{\"tag_name\": \"v${CANDIDATE_VERSION}\",\"target_commitish\": \"master\",\"name\": \"v${CANDIDATE_VERSION}\",\"body\": \"Production release by @${CIRCLE_USERNAME} (via build: [${CIRCLE_BUILD_NUM}](https://circleci.com/gh/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM})).\",\"draft\": false,\"prerelease\": false}" https://api.github.com/repos/${REPO_OWNER}/${CIRCLE_PROJECT_REPONAME}/releases
 	fi
 fi
-# Add a deployment counter in the metrics platform (graphite)
-if [ -z $NO_METRIC ]; then
-	echo "${GRAPHITE_API_KEY}.counters.${APP_NAME}.${ENVIRONMENT}.inf.deployment 1" | nc ${GRAPHITE_ENDPOINT_PREFIX}.carbon.hostedgraphite.com 2003
-fi
