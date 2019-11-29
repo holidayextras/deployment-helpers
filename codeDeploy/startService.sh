5d362d92ee109de65db9735ff6850c67405b547d
Jordan Hood <jordan.hood@virginmedia.com>
2019-11-29 08:53:42 +0000

    update to system ctrl

--- codeDeploy/startService.sh
+++ codeDeploy/startService.sh
@@ -1,15 +1,7 @@
 #!/bin/bash -e
 SERVICE=${APPLICATION_NAME}
 
-if [ ${APPLICATION_NAME} = "themeBlueprint" ] || [ ${APPLICATION_NAME} = "theatreBlueprint" ]; then
-	SERVICE="the-wall"
-fi
-
-file="/etc/init/${SERVICE}.conf"
-if [ ! -f "$file" ]; then
-  # fall back to the default
-  SERVICE="primaryApplication"
-fi
-
 echo "Starting $SERVICE (${APPLICATION_NAME})"
-service $SERVICE start
+
+systemctl start primaryApplication.service
+
--- codeDeploy/stopService.sh
+++ codeDeploy/stopService.sh
@@ -1,15 +1,7 @@
 #!/bin/bash -e
 SERVICE=${APPLICATION_NAME}
 
-if [ ${APPLICATION_NAME} = "themeBlueprint" ] || [ ${APPLICATION_NAME} = "theatreBlueprint" ]; then
-	SERVICE="the-wall"
-fi
-
-file="/etc/init/${SERVICE}.conf"
-if [ ! -f "$file" ]; then
-  # fall back to the default
-  SERVICE="primaryApplication"
-fi
-
 echo "Stopping $SERVICE (${APPLICATION_NAME})"
-service $SERVICE stop
+
+systemctl stop primaryApplication.service
+
